5483d3f17a5b73f5d94823e5a3d8383ab8f138cf
Tomohiko Himura <eiel.hal@gmail.com>
2014-10-11 16:02:59 +0900

    島のページサンプルを作成

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,8 @@
 class Spot < ActiveHash::Base
   self.data = [
     {:id => 1, :description => "朝日が最高"},
-    {:id => 2, :description => "夕日がロマンチック"}
+    {:id => 2, :description => "夕日がロマンチック"},
+    {:id => 3, :description => "夕日がロマンチック"},
+    {:id => 4, :description => "夕日がロマンチック"},
   ]
 end
