3cde0adb74d3f02943dd2f50b04b3d9bdd722ca5
Tomohiko Himura <eiel.hal@gmail.com>
2014-10-11 16:29:09 +0900

    なんか文言修正

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -2,7 +2,7 @@ class Spot < ActiveHash::Base
   self.data = [
     {:id => 1, :description => "朝日が最高"},
     {:id => 2, :description => "夕日がロマンチック"},
-    {:id => 3, :description => "夕日がロマンチック"},
-    {:id => 4, :description => "夕日がロマンチック"},
+    {:id => 3, :description => "安芸の紅葉"},
+    {:id => 4, :description => "瀬戸内の海"},
   ]
 end
